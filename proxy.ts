8540bb147bf21c631008a54690bde8306397a59f
Arthur <45315165+Belster12@users.noreply.github.com>
2026-06-18 04:32:41 +0300

    Add files via upload

---
+++ proxy.ts
@@ -0,0 +1,32 @@
+import {type NextRequest, NextResponse } from 'next/server'
+import { createClient } from './lib/server'
+ 
+export async function proxy(request: NextRequest) {
+  const response = NextResponse.next({
+    request: {
+      headers: request.headers
+    }
+  })
+// 111
+  const supabase = await createClient()
+
+  const {data: {user}} = await supabase.auth.getUser()
+const isAuthPage = request.nextUrl.pathname.startsWith('/auth')
+  if (!user && !isAuthPage) {
+    const url = request.nextUrl.clone()
+    url.pathname = '/auth/login'
+    return NextResponse.redirect(url)
+  }
+
+  if (user && isAuthPage) {
+    const url = request.nextUrl.clone()
+    url.pathname = '/'
+    return NextResponse.redirect(url)
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/((?!auth|_next|api).*)']
+}
